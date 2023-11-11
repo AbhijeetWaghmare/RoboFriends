74a82da676348f5586823de8ac5c064c4257c18d
AbhijeetWaghmare <abhijeetWaghmare2000@gmail.com>
2023-11-11 17:46:30 +0530

    first commit

---
+++ src/robots.js
@@ -0,0 +1,93 @@
+export const robots = [
+    {
+      "id": 1,
+      "name": "John Doe",
+      "email": "johndoe@example.com",
+      "username": "johndoe123"
+    },
+    {
+      "id": 2,
+      "name": "Jane Smith",
+      "email": "janesmith@example.com",
+      "username": "janesmith456"
+    },
+    {
+      "id": 3,
+      "name": "Alice Johnson",
+      "email": "alice@example.com",
+      "username": "alicej"
+    },
+    {
+      "id": 4,
+      "name": "Bob Wilson",
+      "email": "bobwilson@example.com",
+      "username": "bobw"
+    },
+    {
+      "id": 5,
+      "name": "Ella Davis",
+      "email": "elladavis@example.com",
+      "username": "ellad"
+    },
+    {
+      "id": 6,
+      "name": "David Lee",
+      "email": "davidlee@example.com",
+      "username": "davidl"
+    },
+    {
+      "id": 7,
+      "name": "Grace Brown",
+      "email": "gracebrown@example.com",
+      "username": "graceb"
+    },
+    {
+      "id": 8,
+      "name": "Sam Taylor",
+      "email": "samtaylor@example.com",
+      "username": "samt"
+    },
+    {
+      "id": 9,
+      "name": "Olivia White",
+      "email": "oliviawhite@example.com",
+      "username": "oliviaw"
+    },
+    {
+      "id": 10,
+      "name": "William Harris",
+      "email": "williamh@example.com",
+      "username": "williamh123"
+    },
+    {
+      "id": 11,
+      "name": "Sophia Martinez",
+      "email": "sophiam@example.com",
+      "username": "sophiam456"
+    },
+    {
+      "id": 12,
+      "name": "James Robinson",
+      "email": "jamesr@example.com",
+      "username": "jamesr789"
+    },
+    {
+      "id": 13,
+      "name": "Ava Clark",
+      "email": "avaclark@example.com",
+      "username": "avacl"
+    },
+    {
+      "id": 14,
+      "name": "Daniel Baker",
+      "email": "danielb@example.com",
+      "username": "danielb456"
+    },
+    {
+      "id": 15,
+      "name": "Lily Adams",
+      "email": "lilyadams@example.com",
+      "username": "lilya"
+    }
+  ]
+  
